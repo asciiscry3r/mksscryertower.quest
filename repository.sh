a460aafa5adabb98bfc29672ddd39c283cbbfb6d
Maxim Klimenko Sergievich <klimenkomaximsergievich@gmail.com>
2022-10-30 18:38:26 +0200

    Update

--- repository.sh
+++ repository.sh
@@ -3,6 +3,7 @@
 sudo mkdir -p /var/www/repo.mksscryertower.quest/repoupdate
 
 sudo chown arch:arch -R /var/www/repo.mksscryertower.quest/repoupdate
+sudo chown www-data:arch -R /var/www/repo.mksscryertower.quest/repo
 
 aurpackages="ace qt5-styleplugins acpitool alien_package_converter amttool-tng autotiling caffeine-ng cbonsai cpufetch debhelper debtap fotoxx gtk-theme-windows10-dark hardened_malloc hushboard-git i3lock-color icoextract imagewriter imgurbash2 intltool-debian libcurl-openssl-1.0 libestr libxerces-c-3.1 matplotlib-cpp-git mei-amt-check-git modprobed-db numix-circle-icon-theme-git numix-icon-theme-git opencryptoki pa-applet-git picom-tryone-git po-debconf psi-notify pstreams python-pulsectl python-pyasn qt5-styleplugins redeclipse rpi-imager-bin sblim-sfcc sddm-lain-wired-theme sec siji-ttf teiler-git telegram-desktop-bin tpm-tools trousers ttf-unifont tfenv undistract-me-git ventoy-bin wireshark-gtk2 xbindkeys_config-gtk2 xininfo-git xsuspender-git btrfs-assistant flashfocus-git google-chrome inxi mintstick mkinitcpio-openswap networkmanager-support opensnitch-ebpf-module-git opensnitch-git os-prober-btrfs snapper-tools systemd-oomd-defaults timeshift yay-bin yed"
 
@@ -23,7 +24,7 @@ done
 
 repo-add --verify --sign --new repo.mksscryertower.quest.db.tar.gz *.zst
 
-sudo cp -R /var/www/repo.mksscryertower.quest/repo/x86_64 /var/www/repo.mksscryertower.quest/repobackup-$(date +%H%M%m%d%Y)
-sudo rm -rf /var/www/repo.mksscryertower.quest/repo/x86_64
-sudo mv /var/www/repo.mksscryertower.quest/repoupdate /var/www/repo.mksscryertower.quest/repo/x86_64
-sudo chown www-data:arch -R /var/www/repo.mksscryertower.quest/                            
+sudo cp -R /var/www/repo.mksscryertower.quest/repo/x86_64 /var/www/repo.mksscryertower.quest/repo/repobackup-$(date +%H%M%m%d%Y)
+# sudo rm -rf /var/www/repo.mksscryertower.quest/repo/x86_64
+ sudo cp -R -f --remove-destination /var/www/repo.mksscryertower.quest/repoupdate/* /var/www/repo.mksscryertower.quest/repo/x86_64/
+sudo chown www-data:arch -R /var/www/repo.mksscryertower.quest/repo
